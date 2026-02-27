0f33f5f2f79faec1953d17efd801535cde254ab3
Deutsch-Moment <dm.akademie1@gmail.com>
2026-02-27 14:45:02 +0430

    add tiles' logic to interactivities (left click, right click). style tiles' different conditions. add grid layout for tiles' table.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("how is it going?");
+
+const tiles = document.getElementsByClassName('tile');
+for(let tile of tiles) {
+    tile.addEventListener('click', () => {
+        if(!tile.classList.contains('tile--unopened--flagged'))
+            tile.classList.replace('tile--unopened', 'tile--opened');
+    })
+
+    tile.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        let isFlagged = tile.classList.contains('tile--unopened--flagged');
+        let isUnopened = tile.classList.contains('tile--unopened');
+        if(!isFlagged && isUnopened) {
+            tile.classList.replace('tile--unopened', 'tile--unopened--flagged');
+            tile.innerHTML = `<img src="img/flag.png" alt="">`;
+        }
+        else if (isFlagged){
+            tile.classList.replace('tile--unopened--flagged', 'tile--unopened');
+            tile.innerHTML = ``;
+        }
+    })
+}
