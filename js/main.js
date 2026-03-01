4a42730f1569c96033f51fbd1480465fbf6175e7
Deutsch-Moment <dm.akademie1@gmail.com>
2026-03-01 14:50:39 +0430

    feat: add display bombs logic and display timer.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,44 @@ const table = document.getElementById('table');
 const grid = [];
 const level = document.getElementById('selectLevel');
 
+const bombCount = document.getElementById('bomb-count');
+let currentFlag = 0;
+
+const timerDisplay = document.getElementById('timer');
+let timerInterval;
+let isTimerRunning = false;
+let seconds = 0;
+
+function startTimer() {
+    if(isTimerRunning) return;
+    isTimerRunning = true;
+    timerInterval = setInterval(() => {
+        seconds++;
+        updateTimerDisplay();
+    }, 1000);
+}
+
+function resetTimer() {
+    stopTimer();
+    seconds = 0;
+    updateTimerDisplay();
+}
+
+function stopTimer() {
+    isTimerRunning = false;
+    clearInterval(timerInterval);
+}
+
+function updateTimerDisplay() {
+    let mins = Math.floor(seconds / 60).toString().padStart(2,'0');
+    let secs = Math.floor(seconds % 60).toString().padStart(2,'0');
+    timerDisplay.textContent = `${mins}:${secs}`;
+}
+
+function displayRemainingBombs() {
+    bombCount.textContent = grid.totalBombs - currentFlag;
+}
+
 function makeGrid(row) {
     for (let i = 0; i < row; i++) {
         const gridRow = new Array(10).fill(0);
@@ -38,7 +76,6 @@ function designNumbers(tile) {
     if(number === 6) p.style.color = '#B91557';
     if(number === 7) p.style.color = '#9A23A2';
     if(number === 8) p.style.color = '#0F139B';
-    if(number === 9) p.style.color = '#11CEAE';
 }
 
 // Display grid based on the level chosen
@@ -46,16 +83,18 @@ function selectLevel() {
     const selectedLevel = level.value;
     let rows;
     let cols;
+    resetTimer();
+    currentFlag = 0;
 
     if(selectedLevel === 'beginner') {
         rows = 10;
         cols = 10;
-        grid.totalBombs = 13;
+        grid.totalBombs = 12;
     }
     else if(selectedLevel === 'intermediate') {
         rows = 13;
         cols = 10;
-        grid.totalBombs = 13;
+        grid.totalBombs = 22;
     }
     else if(selectedLevel === 'advance') {
         rows = 17;
@@ -70,6 +109,8 @@ function selectLevel() {
     initializeBombs(grid.totalBombs, grid);
     initializeGridCells(grid);
     displayTable(rows,cols);
+    displayRemainingBombs();
+    updateTimerDisplay();
     addEventListeners();
 }
 
@@ -119,6 +160,7 @@ function revealTiles(row, col) {
     tile.classList.add('tile--opened');
 
     if(grid[row][col] === 'b') {
+        stopTimer();
         alert('BOOM! Game Over!');
         revealBombs();
     }
@@ -157,10 +199,12 @@ function addEventListeners() {
     const tiles = document.getElementsByClassName('tile');
     for(let tile of tiles) {
         tile.addEventListener('click', () => {
+            startTimer();
             const r = parseInt(tile.dataset.row);
             const c = parseInt(tile.dataset.col);
             revealTiles(r,c);
             if(isGameOver()) {
+                stopTimer();
                 alert("you won");
                 revealBombs();
             }
@@ -174,10 +218,14 @@ function addEventListeners() {
                 tile.oldContent = tile.innerHTML;
                 tile.classList.replace('tile--unopened', 'tile--unopened--flagged');
                 tile.innerHTML = `<img src="img/flag.png" alt="">`;
+                currentFlag++;
+                displayRemainingBombs();
             }
             else if (isFlagged){
                 tile.classList.replace('tile--unopened--flagged', 'tile--unopened');
                 tile.innerHTML = tile.oldContent;
+                currentFlag--;
+                displayRemainingBombs();
             }
         })
     }
